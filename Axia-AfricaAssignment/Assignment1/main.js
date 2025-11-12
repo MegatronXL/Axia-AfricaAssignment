8654c112d671cd4b3f369575a70e78ffd25c3585
MegatronXL <66179598+MegatronXL@users.noreply.github.com>
2025-11-11 20:16:21 -0800

    initial commit

---
+++ Axia-AfricaAssignment/Assignment1/main.js
@@ -0,0 +1,20 @@
+// Assignment 
+// Nlemadim Pascal Gospel
+// Write a JavaScript program showing the use of 
+// arithmetic, comparison, assignment, and logical
+//  operators with examples and comments where 
+// necessary 
+
+// Arithmetic 
+
+let x = 45;
+let y = 34; // = is an assignment operstor assign 34 to y
+
+const z = x + y; // + is an arithmetic opertor, add two operands
+console.log ("Answer:", z);  
+
+let f = 5;
+let b = 20;
+
+
+
