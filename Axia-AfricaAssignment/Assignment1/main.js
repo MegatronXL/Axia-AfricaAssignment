efd981962e47f9e44ef5fa73902dc04a13a2c305
MegatronXL <66179598+MegatronXL@users.noreply.github.com>
2025-11-13 08:53:57 -0800

    Completed Assignment

--- Axia-AfricaAssignment/Assignment1/main.js
+++ Axia-AfricaAssignment/Assignment1/main.js
@@ -4,17 +4,59 @@
 // arithmetic, comparison, assignment, and logical
 //  operators with examples and comments where 
 // necessary 
+console.log("Welcome to Nlemadim Pascal Assignment");
+alert("Welcome to Nlemadim Pascal Assignment");
 
-// Arithmetic 
-
+ 
 let x = 45;
 let y = 34; // = is an assignment operstor assign 34 to y
 
-const z = x + y; // + is an arithmetic opertor, add two operands
-console.log ("Answer:", z);  
 
-let f = 5;
-let b = 20;
+// Assignment Operator
+// "=" is used to assign 
+let num1 = 23; // assigning num1 to 23
+x -= y; // x -y
+x += y; // x + y
+x /= y; // x / y
+x *= y; // x * y
+// let sum = num1 + num2; // assigning sum to num1  + num2
+let sum2 = "Assignment:";
 
 
+// Arithmetic operators
 
+const rst1 = x + y; // + is an arithmetic opertor for adding operands
+
+const rst6 = sum2 + y; // adding string and number
+const rst2 = x - y; // - is an arithmetic operator for substracting operands
+const rst3 = x * y; // * is an arithmetic operator for multiply operands
+const rst4 = x/y;   // / is an arithmetic operator for dividing operands
+const rst5 = x**y;  //  ** is for arithmetic operator for exponention
+
+
+console.log ("Answer is:", rst1);
+console.log ("Answer is:", rst2);
+console.log ("Answer is:", rst3);
+console.log ("Answer is:", rst4);
+console.log ("Answer is:", rst5);
+console.log ("Answer is:", rst6);
+
+
+// Comparison
+x > y; // greater than
+x < y; // less thannst bool3 = x <= y; // less than or equal to
+x >= y; // greater than or equal to 
+x == y; 
+x === y;
+x != y; // not equal to 
+
+// Logical Operators
+/*
+&& = Logical and
+|| = Logical or
+!  = Logical not
+*/
+
+x = 101 && console.log("Answer is :", x + y);
+y === "29" || y === 34;
+!y === 25; // y is not logical the same as 25
